ae51cebec573b677cb90bd117dc388f86dcf25dd
VirtuK <vitorkuiava.vc@gmail.com>
2024-08-10 00:31:29 -0300

    Consertei o bug do input

--- Assets/Scripts/Forge.cs
+++ Assets/Scripts/Forge.cs
@@ -35,7 +35,7 @@ public class Forge : MonoBehaviour
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-          if (Input.GetKeyDown(KeyCode.E))
+          if (Input.anyKeyDown)
           {
 
                 if (!forgeActive)
